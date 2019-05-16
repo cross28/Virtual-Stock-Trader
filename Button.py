96aa7de05902759a337eea795f542c8c61b7489c
cross28 <icross@lc.edu>
2019-05-16 01:28:58 -0500

    Added a test image instead of a red box

--- Button.py
+++ Button.py
@@ -1,14 +1,15 @@
 import pygame 
 
 class Button(object):
-    def __init__(self, x, y, width, height, win):
+    def __init__(self, x, y, width, height, bg):
         self.x = x
         self.y = y
         self.width = width
         self.height = height 
-        self.win = win
+        self.icon = pygame.image.load(bg)
 
-    def draw(self):
-        pygame.draw.rect(self.win, (255,0,0), (self.x, self.y, self.width, self.height))
+    def draw(self, win):
+        win.blit(self.icon, (self.x, self.y))
+        #pygame.draw.rect(win, (255,0,0), (self.x, self.y, self.width, self.height))
 
     
