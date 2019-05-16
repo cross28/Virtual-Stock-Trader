2d95f772de7a268b3b6552cb68bfbae68991268c
cross28 <icross@lc.edu>
2019-05-16 03:25:47 -0500

    Added a sell button placeholder for testing. Added default and active color parameters to the buttons for hovering. Added some predefined colors

--- Application.py
+++ Application.py
@@ -4,6 +4,13 @@ from Button import Button
 
 pygame.init()
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BRIGHT_RED = (255, 99, 71)
+GREEN = (0, 255, 0)
+BRIGHT_GREEN = (50, 205, 50)
+
 class game(object):
     def __init__(self):
         self.SCREEN_WIDTH = 1000
@@ -11,8 +18,9 @@ class game(object):
         self.win = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         pygame.display.set_caption('Virtual Stock Trader')
 
-        #x, y, width, height, window
-        self.button = Button(50, 50, 50, 50, 'images/buy.jpg')
+        #x, y, width, height, image
+        self.buy_btn = Button(50, 50, 50, 50, GREEN, BRIGHT_GREEN, 'hello')
+        self.sell_btn = Button(50, 100, 50, 50, RED, BRIGHT_RED, 'goodbye')
         
 
     def run(self):
@@ -23,9 +31,30 @@ class game(object):
                 if event.type == pygame.QUIT:
                     run = False
 
-            self.win.fill((255,255,255))
-            self.button.draw(self.win)
+            '''Mouse Events'''
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed()
+
+            #Button interaction
+            if self.buy_btn.x + self.buy_btn.width > mouse[0] > self.buy_btn.x and self.buy_btn.y + self.buy_btn.height > mouse[1] > self.buy_btn.y:
+                self.buy_btn.active(True)
+            else:
+                self.buy_btn.active(False)
+
+            if self.sell_btn.x + self.sell_btn.width > mouse[0] > self.sell_btn.x and self.sell_btn.y + self.sell_btn.height > mouse[1] > self.sell_btn.y:
+                self.sell_btn.active(True)
+            else:
+                self.sell_btn.active(False)
+
+
+            '''Drawing and updating the window'''
+            self.win.fill(WHITE)
+
+            self.buy_btn.draw(self.win)
+            self.sell_btn.draw(self.win)
+
             pygame.display.update()
+
 
         pygame.quit()
 
