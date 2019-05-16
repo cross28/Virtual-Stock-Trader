9055d85305565653c7e4e0a88c07c06e2bf07e13
cross28 <icross@lc.edu>
2019-05-16 13:23:00 -0500

    Deleted some repeated code. Removed hovering functions and added a general mouse action method for detecting when the mouse is hovering over the buttons and when the mouse clicks on the buttons.

--- Button.py
+++ Button.py
@@ -1,7 +1,7 @@
 import pygame 
 
 class Button(object):
-    def __init__(self, x, y, width, height, inactive_color, active_color, text):
+    def __init__(self, x, y, width, height, default_color, hover_color, clicked_color, text):
         self.x = x
         self.y = y
         self.width = width
@@ -9,27 +9,19 @@ class Button(object):
         self.text = text
 
         self.color=()
-        self.inactive_color = inactive_color
-        self.active_color = active_color
-
-    def active(self, a=False):
-        if a is True:
-            self.color = self.active_color
-        else:
-            self.color = self.inactive_color
+        self.default_color = default_color
+        self.hover_color = hover_color
+        self.clicked_color = clicked_color
 
     #Changes colors of mouse is within bounds of the button box
-    def is_hovering(self, mouse):
+    def mouse_actions(self, mouse, click):
         #Button interaction
         if self.x + self.width > mouse[0] > self.x and self.y + self.height > mouse[1] > self.y:
-            self.active(True)
+            self.color = self.hover_color
+            if click == 1:
+                self.color = self.clicked_color
         else:
-            self.active(False)
-
-        if self.x + self.width > mouse[0] > self.x and self.y + self.height > mouse[1] > self.y:
-            self.active(True)
-        else:
-            self.active(False)
+            self.color = self.default_color
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
