18b8d1fb40bf439c9df6aecc2903326878541ca6
cross28 <icross@lc.edu>
2019-05-24 14:28:02 -0500

    Aligned the position of the buy and sell button. Starting to interface with the stock menu

--- Menu.py
+++ Menu.py
@@ -1,7 +1,6 @@
 import pygame
 from Button import Button
 from Stock import Stock
-from Text import Text
 
 RED = (255, 0, 0)
 BRIGHT_RED = (255, 99, 71)
@@ -11,21 +10,22 @@ GREEN = (0, 255, 0)
 BRIGHT_GREEN = (50, 205, 50)
 DARK_GREEN = (0, 100, 0)
 
-BLACK = (0, 0, 0)
 class Menu(object):
     def __init__(self, win, screen_width, screen_height):
         self.win = win
         self.screen_width = screen_width
         self.screen_height = screen_height
+        self.stock = Stock(self.win)
 
-        self.buy_btn = Button(self.win, 50, 50, 50, 50, GREEN, BRIGHT_GREEN, DARK_GREEN, 'Buy')
-        self.sell_btn = Button(self.win, 50, 100, 50, 50, RED, BRIGHT_RED, DARK_RED, 'Sell')
+        self.buy_btn = Button(self.win, self.screen_width / 5, 800, GREEN, BRIGHT_GREEN, DARK_GREEN, 'BUY')
+        self.sell_btn = Button(self.win, 3 * self.screen_width / 5, 800, RED, BRIGHT_RED, DARK_RED, 'SELL')
 
-    def choose_stocks_menu(self):
-        text = Text(self.win, 100,100)
-        text.draw('Hi')
+        companies = ['amazon', 'apple', 'facebook', 'microsoft', 'netflix', 'snapchat', 'tesla', 'twitter']
+        company_data = {}
+        for company in companies:
+            company_img = pygame.image.load('images/{}.png'.format(company))
+
+    def display(self):
         self.buy_btn.draw()
         self.sell_btn.draw()
 
-    #def main_menu(self):
-        
