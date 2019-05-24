b5242503f277b334fcf84990c681f57f4628cbfb
cross28 <icross@lc.edu>
2019-05-24 14:29:10 -0500

    Date time is no longer initialized on object creation. Added a fixed company list for simplicity. Moved the stock data api to its own method

--- Stock.py
+++ Stock.py
@@ -5,13 +5,16 @@ import datetime as dt
 api_key = open('secret.txt', 'r').read()
 
 class Stock(object):
-    def __init__(self, companyList=[]):
+    def __init__(self, win):
+        self.win = win
+        self.companyList = ['AMZN', 'AAPL', 'FB', 'MSFT', 'NFLX', 'SNAP', 'TSLA', 'TWTR']
+    
+    def getStockData(self):
         day = dt.date.today()
-        self.companyList = companyList
-        self.companyPrices = {}
-        for company in companyList:
+        for company in self.companyList:
             url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize=full&apikey='.format(company) + api_key
             data = re.get(url).json()
             data = data["Time Series (Daily)"][str('2019-05-15')]
             self.companyPrices[company] = data
         self.companyPrices = json.dumps(self.companyPrices, indent = 2)
+        
