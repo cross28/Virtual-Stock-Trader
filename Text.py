78c811e78e922d584c6ce34aa8faad249f7d222c
cross28 <icross@lc.edu>
2019-05-24 14:27:06 -0500

    Got rid of the get height and get width methods for the text box.

--- Text.py
+++ Text.py
@@ -1,24 +1,22 @@
 import pygame
-import pygame.freetype
 
 '''Will handle text output to the menu'''
 class Text(object):
     msg = None
-    
-    def __init__(self, win, x, y, size=30):
-        self.font = pygame.font.SysFont('Arial.ttf', size)
-        self.win = win
 
+    def __init__(self, win, x, y, size=30):
+        self.font = pygame.font.Font('Arial.ttf', size)
+        self.win = win
         self.x = x
         self.y = y 
 
-    def draw(self, msg):
+    def draw(self, msg, inBox = False):
         self.msg = msg
-        self.text = self.font.render(self.msg, 1, (0,0,0))
-        self.win.blit(self.text, (self.x, self.y))
+        text = self.font.render(self.msg, 1, (0,0,0))
+        self.win.blit(text, (self.x, self.y))
 
-    def get_width(self):
-        return 20
-
-    def get_height(self):
-        return 20
+        # The inBox boolean is for buttons.
+        # Will be true if text is for button
+        if inBox is True:
+            self.x = text.get_rect().width / 2
+            self.y = text.get_rect().height / 2
