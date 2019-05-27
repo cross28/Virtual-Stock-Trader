de0b76694c2f907ce1291f8acf78877bea52894b
cross28 <icross@lc.edu>
2019-05-27 15:00:42 -0500

    A box will now appear around the stock if the mouse if hovering over it. Will change colors if clicked.

--- Stock.py
+++ Stock.py
@@ -5,13 +5,28 @@ class Stock(object):
         self.win = win
         self.x = x
         self.y = y
+        self.width = 150
+        self.height = 150
 
         self.data = {}
         self.data = data
 
-        self.img = pygame.image.load(img)
-        self.img = pygame.transform.scale(self.img, (150, 150))
+        self.img = pygame.transform.scale(img, (self.width, self.height))
+
+    def events(self):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()[0]
+
+        if self.x + self.width > mouse[0] > self.x and self.y + self.height > mouse[1] > self.y:
+            pygame.draw.rect(self.win, (0, 0, 255), (self.x - 25, self.y - 25, self.width + 50, self.height + 50))
+            self.win.blit(self.img, (self.x, self.y))
+            if click == 1:
+                pygame.draw.rect(self.win, (0, 100, 255), (self.x - 25, self.y - 25, self.width + 50, self.height + 50))
+                self.win.blit(self.img, (self.x, self.y))
+        else:
+            self.win.blit(self.img, (self.x, self.y))
 
     def draw(self):
+        self.events()
         self.win.blit(self.img, (self.x, self.y))     
         
