15b2b333919a1e523642af7cbe8b9860e67291dc
cross28 <icross@lc.edu>
2019-05-27 05:38:08 -0500

    Whitespace changes

--- Game.py
+++ Game.py
@@ -2,8 +2,9 @@ import pygame
 from Stock import Stock
 from Button import Button
 from Menu import Menu
+
+
 WHITE = (255, 255, 255)
-pygame.init()
 
 class game(object):
     def __init__(self):
@@ -11,29 +12,26 @@ class game(object):
         self.SCREEN_HEIGHT = 1000
         self.win = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         pygame.display.set_caption('Virtual Stock Trader')
-
         self.menu = Menu(self.win, self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
-        
         
     def run(self):
         run = True
         while run:
-            
+            '''Window Events'''
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
-
             '''Drawing and updating the window'''
             self.win.fill(WHITE)
-            self.menu.display()
+            self.menu.displayBuySellMenu()
             pygame.display.update()
 
 
         pygame.quit()
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
+    pygame.init()
     g = game()
     g.run()
