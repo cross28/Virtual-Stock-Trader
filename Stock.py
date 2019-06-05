21fedc7946c8c981c4219c65accec2634f8a55ac
cross28 <icross@lc.edu>
2019-06-05 10:00:59 -0500

    Clicking on the stock images doesnt return true or false. Passing the buy and sell menu as a parameter to the draw function to add faux interactability.

--- Stock.py
+++ Stock.py
@@ -2,15 +2,13 @@ import pygame
 import time
 from Text import Text
 
-class Stock(pygame.sprite.Sprite):
+class Stock(object):
     def __init__(self, win, x, y, img, data={}):
-        pygame.sprite.Sprite.__init__(self)
         self.win = win
         self.x = x
         self.y = y
         self.width = 150
         self.height = 150
-
         self.data = {}
         self.data = data['1. open']
         self.data = round(float(self.data), 2)
@@ -18,23 +16,14 @@ class Stock(pygame.sprite.Sprite):
         self.img = pygame.transform.scale(img, (self.width, self.height))
         self.text = Text(self.win, self.x + self.width / 2 - 40, self.y + self.height + 50, '{0:.2f}'.format(self.data), bold=True, font='comicsansms')
 
-    def mouseEvents(self):
+    def draw(self,action=None):
         mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()[0]
-
+        click = pygame.mouse.get_pressed()
+        
         if self.x + self.width > mouse[0] > self.x and self.y + self.height > mouse[1] > self.y:
             pygame.draw.rect(self.win, (0, 0, 255), (self.x - 25, self.y - 25, self.width + 50, self.height + 50))
-            self.win.blit(self.img, (self.x, self.y))
-            if click == 1:
-                pygame.draw.rect(self.win, (0, 100, 255), (self.x - 25, self.y - 25, self.width + 50, self.height + 50))
-                self.win.blit(self.img, (self.x, self.y))
-                return True
-            else:
-                return False
+            if click[0] == 1 and action!=None:
+                action()                   
         else:
-            self.win.blit(self.img, (self.x, self.y)) 
-        
-    def draw(self):
-        self.mouseEvents()
-        self.win.blit(self.img, (self.x, self.y))
+            self.win.blit(self.img, (self.x, self.y))
         self.text.draw()
