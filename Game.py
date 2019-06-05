3a2282becf6ae1784c9214742976896e86da9251
cross28 <icross@lc.edu>
2019-06-05 09:57:48 -0500

    Added the different menus to Game.py and deleted the Menu.py file.

--- Game.py
+++ Game.py
@@ -1,33 +1,94 @@
 import pygame
-from Menu import Menu
+from Button import Button
+from Text import Text
+from Stock import Stock
+
+import requests as re 
+import json
+import datetime as dt 
+import time
+
+RED = (255, 0, 0)
+BRIGHT_RED = (255, 99, 71)
+DARK_RED = (139, 0, 0)
+GREEN = (0, 255, 0, 0.5)
+BRIGHT_GREEN = (50, 205, 50)
+DARK_GREEN = (0, 100, 0)
+WHITE = (255, 255, 255)
+
+pygame.init()
+SCREEN_WIDTH = 1500
+SCREEN_HEIGHT = 1000
+win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Virtual Stock Trader')
+
+'''
+#Grabbing Stock Data
+api_key = open('secret.txt', 'r').read()
+day = dt.date.today()
+companyList = ['AMZN', 'AAPL', 'FB', 'MSFT', 'NFLX']
+companyPrices = {}
+for company in companyList:
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize=full&apikey='.format(company) + api_key
+    data = re.get(url).json()['Time Series (Daily)']['2019-05-24']
+    companyPrices[company] = data
+'''
+
+#Pre-loading stock images
+fb = pygame.image.load('images/facebook.png')
+amzn = pygame.image.load('images/amazon.png')
+nflx = pygame.image.load('images/netflix.png')
+appl = pygame.image.load('images/apple.png')
+msft = pygame.image.load('images/microsoft.png')
+
+#Creating buttons
+money = Text(win, 20, 40, '50,000', size=40)
+buy_btn = Button(win, SCREEN_WIDTH / 5, 800, GREEN, BRIGHT_GREEN, DARK_GREEN, 'Buy')
+sell_btn = Button(win, 3 * SCREEN_WIDTH / 5, 800, RED, BRIGHT_RED, DARK_RED, 'Sell')
+
+companyPrices = {'FB':{'1. open': 20}, 'AMZN':{'1. open': 20}, 'AAPL':{'1. open': 20}, 'MSFT':{'1. open':20}, 'NFLX':{'1. open': 20}}
+facebook = Stock(win, 30, 300, fb, companyPrices['FB'])
+amazon = Stock(win, SCREEN_WIDTH / 5, 300, amzn, companyPrices['AMZN'])
+apple = Stock(win, 2 * SCREEN_WIDTH / 5, 300, appl, companyPrices['AAPL'])
+microsoft = Stock(win, 3 * SCREEN_WIDTH / 5, 300, msft, companyPrices['MSFT'])
+netflix = Stock(win, 4.2 * SCREEN_WIDTH / 5, 300, nflx, companyPrices['NFLX'])
+stocks = [facebook, amazon, apple, microsoft, netflix]
+
+def buySellMenu():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+
+        win.fill(WHITE)
+        buy_btn.draw()
+        sell_btn.draw()
+        pygame.display.update()
+
+def mainMenu():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+        
+        win.fill(WHITE)
+        for stock in stocks:
+            stock.draw(buySellMenu)
+        pygame.display.update()
 
 
-class game(object):
-    def __init__(self):
-        self.SCREEN_WIDTH = 1500
-        self.SCREEN_HEIGHT = 1000
-        self.win = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
-        pygame.display.set_caption('Virtual Stock Trader')
-        self.menu = Menu(self.win, self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
+mainMenu()
 
-    def run(self):
-        run = True
-        while run:
-            '''Window Events'''
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        run = False
-            '''Drawing and updating the window'''
-            self.win.fill((255, 255, 255))
-            self.menu.mainMenu()
-            pygame.display.update()
-
-
-if __name__ == '__main__':
-    pygame.init()
-    g = game()
-    g.run()
-    pygame.quit()
+pygame.quit()
