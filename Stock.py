ba69297fa59d8b11e63e58dde13f4c4fe7e4f54b
cross28 <icross@lc.edu>
2019-06-15 14:36:33 -0500

    Added a drawselected method. If the stock is chosen, it with display the buy/sell menu and the image of the chosen stock along with the stock price and stocks owned

--- Stock.py
+++ Stock.py
@@ -14,7 +14,7 @@ class Stock(object):
         self.data = data['1. open']
         self.data = round(float(self.data), 2)
 
-        self.img = pygame.transform.scale(img, (self.width, self.height))
+        self.img = pygame.transform.scale(pygame.image.load(img), (self.width, self.height))
         self.priceText = Text(self.win, self.x + self.width / 2 - 40, self.y + self.height + 50, 'Price: {0:.2f}'.format(self.data), font='comicsansms')
         self.stocksOwnedText = Text(self.win, self.priceText.x, self.priceText.y + 30, 'Own: {}'.format(self.stocksOwned), font='comicsansms')
 
@@ -23,7 +23,12 @@ class Stock(object):
     def draw(self,action=None):
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()[0]
-        
+
+        #Resetting x, y, width, and height values 
+        self.img = pygame.transform.scale(self.img, (self.width, self.height))
+        self.priceText.x, self.priceText.y = self.x + self.width / 2 - 40, self.y + self.height + 50
+        self.stocksOwnedText.x, self.stocksOwnedText.y = self.priceText.x, self.priceText.y + 30
+
         if self.x + self.width > mouse[0] > self.x and self.y + self.height > mouse[1] > self.y:
             pygame.draw.rect(self.win, (0, 0, 255), (self.x - 25, self.y - 25, self.width + 50, self.height + 50))
             self.win.blit(self.img, (self.x, self.y))
@@ -40,3 +45,18 @@ class Stock(object):
     def stocksChange(self, number):
         self.stocksOwned += number
         self.stocksOwnedText.changeMessage('Own: {}'.format(self.stocksOwned))
+        
+    #If the stock was chosen, this will transform the image and draw it
+    def drawSelected(self):
+        x = 900
+        y = 250
+
+        self.img = pygame.transform.scale(self.img, (250, 250))
+        self.stocksOwnedText.x, self.stocksOwnedText.y = x + 250, y + 80
+        self.priceText.x, self.priceText.y = x + 250, y + 50
+
+        self.win.blit(self.img, (x, y))
+        self.stocksOwnedText.draw()
+        self.priceText.draw()
+
+
